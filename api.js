efc7bfd65be4221581b05055d3f10237d67f9bba
solutionsuberaba <matheus.solutions@icloud.com>
2026-04-07 21:26:39 -0300

    rota estatica corrigida para frontend

--- api.js
+++ api.js
@@ -24,7 +24,7 @@ const app = express();
 // =========================================================================
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Estabelecendo Conexão com o Banco de Dados Central Solutions QR
 const db = new sqlite3.Database(process.env.DB_PATH || path.join(__dirname, 'database.db'), (err) => {
